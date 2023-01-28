25e3bfb05008e0d6549dbc711c4848b4f06c3a20
midnyt <theplashrz@gmail.com>
2023-01-28 18:50:09 +0530

    update 1

--- src/liveClass.java
+++ src/liveClass.java
@@ -1,8 +1,33 @@
-public class liveClass {
+import java.util.Calendar;
 
+public class liveClass {
+    //Static block
+    static {
+
+        System.out.println("world");
+    }
     public static void main(String[] args){
 
         System.out.println("Hello");
 
+        char k = 'a'+1;
+        k = (char) (k + 2);
+        System.out.println(k);
+
+        outPuty();
+
+        liveClass l = new liveClass();
+        l.outPuty1();
+
+        Calendar cal = Calendar.getInstance();
+        System.out.println(cal.getTime());
+
+    }
+    public static void outPuty (){
+        System.out.println("hello");
+    }
+
+    public void outPuty1 (){
+        System.out.println("hello");
     }
 }
