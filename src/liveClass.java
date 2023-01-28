c8e0a3e8d00c58ac05fb60b343b69668c47e0512
midnyt <theplashrz@gmail.com>
2023-01-28 23:22:20 +0530

    updated

--- src/liveClass.java
+++ src/liveClass.java
@@ -8,19 +8,19 @@ public class liveClass {
     }
     public static void main(String[] args){
 
-        System.out.println("Hello");
+       // System.out.println("Hello");
 
-        char k = 'a'+1;
-        k = (char) (k + 2);
-        System.out.println(k);
+        //char k = 'a'+1;
+       // k = (char) (k + 2);
+      //  System.out.println(k);
 
-        outPuty();
+      //  outPuty();
 
-        liveClass l = new liveClass();
-        l.outPuty1();
+      //  liveClass l = new liveClass();
+      //  l.outPuty1();
 
-        Calendar cal = Calendar.getInstance();
-        System.out.println(cal.getTime());
+     //   Calendar cal = Calendar.getInstance();
+     //   System.out.println(cal.getTime());
 
     }
     public static void outPuty (){
--- src/liveClass2.java
+++ src/liveClass2.java
@@ -5,16 +5,11 @@ public class liveClass2 extends liveClass {
         liveClass obj1 = new liveClass2();
         obj1.outPuty1();
 
-        //liveClass2 obj2 = (liveClass2) obj1;
-       // obj2.outPuty1();
-        //liveClass.outPuty();
-       //super.outPuty();
-
 
     }
 
     public void outPuty1 (){
         System.out.println("hello from class 2");
-        //super.outPuty();
+        super.outPuty1();
     }
 }
