2571e21cbae3b44ab61a825e53a271566b571081
midnyt <theplashrz@gmail.com>
2023-01-28 23:32:24 +0530

    few ways to do

--- src/liveClass2.java
+++ src/liveClass2.java
@@ -4,12 +4,16 @@ public class liveClass2 extends liveClass {
 
         liveClass obj1 = new liveClass2();
         obj1.outPuty1();
+        //  liveClass2 x = (liveClass2) obj1;
+        // x.outPuty1();
+        liveClass obj2 = new liveClass();
+        obj2.outPuty1();
 
 
     }
 
     public void outPuty1 (){
         System.out.println("hello from class 2");
-        super.outPuty1();
+        //super.outPuty1();
     }
 }
